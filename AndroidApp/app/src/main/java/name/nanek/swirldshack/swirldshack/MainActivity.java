d45ef9fabf2ece7183f50d2ae15bf45b6165a8bd
Lance Nanek <lnanek@snapchat.com>
2017-09-16 14:40:04 -0700

    Android app attempt. Currently has dex error due to Java 1.8 deps.

---
+++ AndroidApp/app/src/main/java/name/nanek/swirldshack/swirldshack/MainActivity.java
@@ -0,0 +1,13 @@
+package name.nanek.swirldshack.swirldshack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
