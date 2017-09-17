09002d2591bb066c856015677fa746c3c65583ae
Lance Nanek <lnanek@snapchat.com>
2017-09-16 20:22:40 -0700

    Starting unity app. Empty app that just logs to console on start.

---
+++ UnityApp/SwirldsHack/Assets/PostToHashGraph.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostToHashGraph : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Lance was here");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
